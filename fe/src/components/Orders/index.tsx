1078c36f28443a618e353cff3823af0d36de31cd
Kaio <kaiocarvalho558@gmail.com>
2024-05-22 14:58:59 -0300

    Fix: correção de bug de duplicação

--- fe/src/components/Orders/index.tsx
+++ fe/src/components/Orders/index.tsx
@@ -1,66 +1,72 @@
 import { useEffect, useState } from "react";
-import socket from 'socket.io-client';
 import { Order } from "../../types/Order";
 import { api } from "../../utils/api";
 import { OrdersBoard } from "../OrdearsBoard";
 import { Container } from "./styles";
 
-export function Orders() {
-  const [orders, setOrders] = useState<Order[] | []>([]);
+import socketIo from 'socket.io-client';
 
-  useEffect(() => {
-    api.get('orders').then(({ data }) => setOrders(data));
+export function Orders (){
+  const [orders, SetOrders] = useState<Order[]>([]);
+
+  useEffect(() =>{
+  const socket = socketIo('http://localhost:3001', {
+  transports: ['websocket'],
+
   });
 
+  socket.on('orders@new', (order) =>{
+    SetOrders(prevState => prevState.concat(order));
+  });
+  },[]);
+
   useEffect(() => {
-    const io = socket('http://localhost:5000/', {
-      transports: ['websocket']
-    });
-    io.on('order@new', (order) => setOrders(prev => prev.concat(order)))
-  }, []);
+   api.get('/orders')
+    .then(({data}) => {
+      SetOrders(data);
+    })
+  })
 
-  const waiting = orders.filter(({ status }) => status === 'WAITING');
-  const inProduction = orders.filter(({ status }) => status === 'IN_PRODUCTION');
-  const done = orders.filter(({ status }) => status === 'DONE');
+  const waiting = orders.filter((order) => order.status === 'WAITING');
+  const inProduction = orders.filter((order) => order.status === 'IN_PRODUCTION');
+  const done = orders.filter((order) => order.status === 'DONE');
 
-  function handleCancelOrder(orderId: string) {
-    setOrders((prev) => prev.filter(({_id}) => _id !== orderId));
-  }
+function handleCancelOrder(orderId: string) {
+  SetOrders((prevState) => prevState.filter(order => order._id !== orderId))
+}
 
-  function handleOrderStatusChange(orderId: string, status: Order['status']) {
-    setOrders((prev) => prev.map((order) => {
-      if (order._id !== orderId) return {
-        ...order,
-        status
-      };
+function handleOrderStatusChange(orderId: string, status: Order['status']){
+  SetOrders((prevState) => prevState.map((order) => (
+    order._id === orderId
+    ? {...order, status }
+    : order
 
-      return order;
-    }));
-  }
+  )));
+}
 
-  return (
-    <Container>
-      <OrdersBoard
-        icon="🕑"
-        title="Fila de espera"
-        orders={waiting}
-        onCancelOrder={handleCancelOrder}
-        onChangeOrderStatus={handleOrderStatusChange}
-      />
-      <OrdersBoard
-        icon="👩‍🍳"
-        title="Em preparação"
-        orders={inProduction}
-        onCancelOrder={handleCancelOrder}
-        onChangeOrderStatus={handleOrderStatusChange}
-      />
-      <OrdersBoard
-        icon="✅"
-        title="Pronto!"
-        orders={done}
-        onCancelOrder={handleCancelOrder}
-        onChangeOrderStatus={handleOrderStatusChange}
-      />
-    </Container>
-  );
+return (
+  <Container>
+    <OrdersBoard
+    icon="🕛"
+    title="Fila de espera"
+    orders={waiting}
+    onCancelOrder={handleCancelOrder}
+    onChangeOrderStatus={handleOrderStatusChange}
+    />
+    <OrdersBoard
+     icon="👨🏽‍🍳"
+     title="Em preparação"
+     orders={inProduction}
+     onCancelOrder={handleCancelOrder}
+     onChangeOrderStatus={handleOrderStatusChange}
+    />
+    <OrdersBoard
+     icon="✅"
+     title="Pronto!"
+     orders={done}
+     onCancelOrder={handleCancelOrder}
+     onChangeOrderStatus={handleOrderStatusChange}
+    />
+  </Container>
+ )
 }
