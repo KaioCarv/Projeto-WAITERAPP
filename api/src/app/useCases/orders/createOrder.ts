22aefd3b3129a801f02543a762aa0d6d6fd2c4ec
Kaio <kaiocarvalho558@gmail.com>
2024-05-22 00:32:26 -0300

    Integração do front-end web com a API + implementação dos WebSockets

--- api/src/app/useCases/orders/createOrder.ts
+++ api/src/app/useCases/orders/createOrder.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { io } from "../../../index";
 import { Order } from "../../models/Order";
 
 
@@ -8,7 +9,9 @@ export async function createOrder(req: Request,res: Response){
 
 
     const order = await Order.create({ table, products });
+    const orderDetails = await order.populate('products.product')   ;
 
+    io.emit('orders@new', orderDetails)
     res.status(201).json(order);
    } catch (error) {
     console.log(error);
